baf741ff14f70a2f5befa7effc3b19de925a6882
oguzhankirikkk <osman.oguzhan544@gmail.com>
2024-05-19 15:41:27 +0300

    giriş syafası güncelleme hata

--- giris/index.php
+++ giris/index.php
@@ -1,16 +1,31 @@
 <?php
-$username=$_POST['username'];
-$password=$_POST['password'];
 
- if($username=='b231210372@sakarya.edu.tr'||$password=='b231210372')
-    {    
-        
-        header('Location: https://webproje.vercel.app/giris/dogru.html');
- 
-    }
-    
-    else
-    header('Location: https://webproje.vercel.app/giris/index.html');
+session_start();
+ob_start();
 
 
+if(($_POST["user"]=="B231210372@ogr.sakarya.edu.tr") && ($_POST["pass"]=="B231210372"))
+{
+
+$_SESSION["login"] = "true";
+$_SESSION["user"] = "B231210372@ogr.sakarya.edu.tr";
+$_SESSION["pass"] = "B231210372";
+
+echo "Hosgeldiniz. Giriş basarili. Anasayfaya yonlendiriliyorsunuz. Lütfen bekleyin..";
+header("Refresh:2; url=dogru.html");
+
+}
+elseif(($_POST["user"]=="") && ($_POST["pass"]==""))
+{
+echo "Kullanici adi veya sifre bos birakilamaz.";
+header("Refresh: 2; url=index.html");
+}
+else
+{
+echo "Kullanici adi veya sifre yanlis.";
+header("Refresh: 2; url=index.html");
+}
+
+
+ob_end_flush();
 ?>
