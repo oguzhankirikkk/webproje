d1fdfa39357eb4920c4deb1c44d7400d6e47efa8
oguzhankirikkk <osman.oguzhan544@gmail.com>
2024-05-19 21:51:21 +0300

    son degisiklikler yapildi

--- giris/index.php
+++ giris/index.php
@@ -1,31 +1,30 @@
 <?php
-
 session_start();
 ob_start();
+$user = isset($_POST['user']);
+echo $user;
+echo "eeeee";
 
 
-if(($_POST["user"]=="B231210372@ogr.sakarya.edu.tr") && ($_POST["pass"]=="B231210372"))
-{
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Kullanıcı adı ve şifrenin gönderilip gönderilmediğini kontrol edin
+    $user = isset($_POST['user']) ? $_POST['user'] : '';
+    $pass = isset($_POST['pass']) ? $_POST['pass'] : '';
 
-$_SESSION["login"] = "true";
-$_SESSION["user"] = "B231210372@ogr.sakarya.edu.tr";
-$_SESSION["pass"] = "B231210372";
+    if ($user == "B231210372@ogr.sakarya.edu.tr" && $pass == "b231210372") {
+        $_SESSION["login"] = "true";
+        $_SESSION["user"] = "b231210372@ogr.sakarya.edu.tr";
+        $_SESSION["pass"] = "b231210372";
 
-echo "Hosgeldiniz. Giriş basarili. Anasayfaya yonlendiriliyorsunuz. Lütfen bekleyin..";
-header("Refresh:2; url=dogru.html");
-
+        echo "Hoşgeldiniz. Giriş başarılı. Anasayfaya yönlendiriliyorsunuz. Lütfen bekleyin..";
+        header("Refresh:2; url=dogru.html");
+    } elseif (empty($user) || empty($pass)) {
+        echo "Kullanıcı adı veya şifre boş bırakılamaz.";
+        header("Refresh: 2; url=index.html");
+    } else {
+        echo "Kullanıcı adı veya şifre yanlış.";
+        header("Refresh: 2; url=index.html");
+    }
 }
-elseif(($_POST["user"]=="") && ($_POST["pass"]==""))
-{
-echo "Kullanici adi veya sifre bos birakilamaz.";
-header("Refresh: 2; url=index.html");
-}
-else
-{
-echo "Kullanici adi veya sifre yanlis.";
-header("Refresh: 2; url=index.html");
-}
-
-
 ob_end_flush();
 ?>
